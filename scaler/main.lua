942eed9dcd88691f43b53b5c180f043d658684a9
spritebeast <spritebeast@gmail.com>
2021-12-22 01:49:16 +0100

    Draw
    Draw background and player

--- scaler/main.lua
+++ scaler/main.lua
@@ -7,12 +7,29 @@ function love.load()
     love.window.setMode(window.width, window.height)
     love.window.setTitle(window.title)
     love.graphics.setDefaultFilter("nearest", "nearest")
+
+    bg = {}
+    bg.img = love.graphics.newImage("grass32.png")
+    background = {}
+
+    player = {}
+    player.img = love.graphics.newImage("spritebeast32.png")
+    player.x = window.width/2
+    player.y = window.height/2
+
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+    for y = #background, window.height, 96 do
+        for x = #background, window.width, 96 do
+            love.graphics.draw(bg.img, x, y, nil, 3, 3)
+        end
+    end
+
+    love.graphics.draw(player.img, player.x, player.y, nil, player.size, player.size, player.img:getWidth()/2, player.img:getHeight()/2)
 end
 
 function love.keypressed(key)
